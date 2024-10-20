eeb353665ab5fb6ed66f657c459df4ed7316a259
apurvev <53811762+apurvev@users.noreply.github.com>
2024-10-19 20:44:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-   region = "us-east-1"
-   access_key = var.access_key
-   secret_key = var.secret_key
-  
-}
 
 resource "aws_instance" "new_ins" {
     ami = "ami-066784287e358dad1"
